0ecd896df4967376f964f260f00d84d89b63f15d
ejonghwan <sun87-1@daum.net>
2020-03-28 21:07:48 +0900

    'first'

--- src/List.js
+++ src/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const List = ( {todos, loadings} ) => {
     let list = <div>loadings...</div>;
-    if(!loadings) return todos.map( data => <li key={data.id}>{data.title}</li>)
+    if(!loadings) return todos.map( data => <li key={data.id}>{data.id}  - {data.title}</li>)
     return (
         <ul>
            {list}
