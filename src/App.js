99ff5f911af59028e977399cd707e564787d3a7a
ejonghwan <sun87-1@daum.net>
2020-03-28 11:21:39 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import List from './List.js'
 
 function App() {
+const [todo, setTodo] = useState(['start']);
+const [inputValue, setInputValue] = useState();
+const [loading, setLoading] = useState(false);
+
+const handleChange = (e) => {
+  setInputValue(e.target.value);
+}
+
+const createData = (e) => { //넣어주는애
+  e.preventDefault();
+  setTodo([...todo, {title:inputValue, id:todo.length}])
+}
+
+const handleData = async () => { //받아온 패칭데이터를 지금 존재하는 todo 데이터에 넣음 
+  setLoading(true)
+  const data = await fetch('http://jsonplaceholder.typicode.com/todos')
+  const dataJson = await data.json();
+  setTodo(dataJson)
+  console.log(dataJson)
+  setLoading(false)
+}
+
+
+useEffect( () => {
+  console.log('new render')
+}, [todo] )
+
+useEffect( () => {
+  handleData()
+}, [] )
+
+
   return (
-    <div className="App">
-     asdasdasdasd
+    <div>
+       <form>
+          <input onChange={handleChange} />
+          <button onClick={createData}>create</button>
+        </form>
+     <List todos={todo} loadings={loading} />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const List = ( {todos, loadings} ) => {
+    let list = <div>loadings...</div>;
+    if(!loadings) return todos.map( data => <li key={data.id}>{data.title}</li>)
+    return (
+        <ul>
+           {list}
+        </ul>
+    )
+} 
+
+export default List;
