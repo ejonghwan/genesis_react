7971d390b6f5c92934ab0588bd9272d487a31253
ejonghwan <sun87-1@daum.net>
2023-07-25 17:31:09 +0900

    메인 비주얼 수정

--- src/components/common/list/ImgListItem.js
+++ src/components/common/list/ImgListItem.js
@@ -4,14 +4,11 @@ const ImgListItem = ({ data }) => {
     const { itemClassName, src, webp, alt, href, pointEvt, loading } = data;
     return (
         <div className={itemClassName}>
-            <a href={href} role="button" style={{ pointerEvents: pointEvt }}>
-                {/* <img src={src} alt={alt} loading={loading} /> */}
-                <Img 
-                    webpSrc={webp}
-                    imageSrc={src}
-                    alt={alt}
-                />
-            </a>
+            <Img 
+                webpSrc={webp}
+                imageSrc={src}
+                alt={alt}
+            />
         </div>
     );
 };
