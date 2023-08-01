44d7262f8d37a2f5db5cfbc42d9c091892374d48
ejonghwan <sun87-1@daum.net>
2023-08-01 12:35:06 +0900

    맵 리사이즈 센터 스로틀링 적용

--- src/components/sub/Maps.js
+++ src/components/sub/Maps.js
@@ -86,26 +86,17 @@ const Maps = ({ mapData }) => {
 			? MapIns?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: MapIns?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [MapIns, Traffic, kakao]);
-
-	const mapCenter = e => {
-		console.log('map c??')
-	}
+	const mapCenter = e => MapIns?.panTo( new kakao.maps.LatLng(TargetLatLng.lat, TargetLatLng.long) );
+		
 
 	useEffect(() => {
 		window.addEventListener('resize', _.debounce(mapCenter, 500))
-
+		
 		return () => {
 			window.removeEventListener('resize', _.debounce(mapCenter, 500))
 		};
+	}, [MapIns])
 
-	}, [])
-
-
-    // 리사이즈시 센터로
-    // window.addEventListener('resize', ui.lastCallDebounce(
-    //         () => handleResize(map, new kakao.maps.LatLng(region_info[map_idx].letlong.lat, region_info[map_idx].letlong.long)), 
-    //         300 
-    //     ))
 
 
 
