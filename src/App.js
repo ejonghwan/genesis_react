8628d69ab46c076125b671b6bcc3ca85584bb987
ejonghwan <sun87-1@daum.net>
2023-08-03 12:11:57 +0900

    seo 작업 helmet async 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Fragment, lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import HeadMetaTag from '../src/components/common/HeadMetaTag';
 
 //common
 import Footer from './components/common/Footer';
@@ -16,12 +17,25 @@ const Gallery = lazy(() => import('./pages/Gallery'));
 const Youtube = lazy(() => import('./pages/Youtube'));
 
 
-
 const App = () => {
+
+	const meta = {
+		title: "제네시스(GENESIS) 공식 프로모션 웹사이트 | GENESIS Korea 제네시스(GENESIS) 공식 프로모션 웹사이트 | GENESIS Korea", 
+		description: "역동적인 우아함과 진보된 기술을 바탕으로 럭셔리 자동차의 새로운 기준을 제시합니다. 세단, SUV, 전기차, 컨셉카 등 제네시스의 다양한 모델을 만나보세요. | 제네시스 공식 웹사이트 제네시스(GENESIS) 공식 웹사이트 | GENESIS Korea", 
+		keywords: "세단, SUV, 전기차, 컨셉카 등 제네시스의 다양한 모델을 만나보세요. | 제네시스 공식 웹사이트 제네시스(GENESIS) 공식 웹사이트 | GENESIS Korea", 
+		imgsrc: "https://www.genesis.com/content/dam/genesis-p2/kr/assets/utility/sns/genesis-kr-logo-social-1200x630-ko.jpg", 
+		url: "https://ejonghwan.github.io/genesis_react/",
+	}
 
 	return (
 		<Fragment>	
-
+			<HeadMetaTag 
+				title={meta.title} 
+				description={meta.description}
+				keywords={meta.keywords}
+				imgsrc={meta.imgsrc}
+				url={meta.url}
+			/>
 			<Switch>
 				{/* <Route exact path='/' component={Main} /> */}
 				<Route exact path='/' render={() => <Main />} /> 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 
 import store from './redux/store'; 
 
@@ -10,7 +11,9 @@ import store from './redux/store';
 ReactDOM.render(
 	<HashRouter>
 		<Provider store={store}>
-			<App />
+			<HelmetProvider>
+				<App />
+			</HelmetProvider>
 		</Provider>
 	</HashRouter>,
 	document.getElementById('root')
